c66cab88327f5c9934d5449fd76108b0f2cc06b3
Cameron <Cameron@dansclan.com>
2023-03-15 21:16:44 -0700

    Added Header, Projects, Footer, styling

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#Home">Home</a></li>
+          <li><a href="#Projects">Projects</a></li>
+          <li><a href="#Experience">Experience</a></li>
+          <li><a href="#Educatiom">Education</a></li>
+        </ul>
+      </nav>
+      <div className="header-details">
+        <h1>Cameron Millen</h1>
+        <h2>Full-Stack Application Developer</h2>
+        <ul>
+          <li><a href='https://goo.gl/maps/P7a7UhE47pvPLAdm9'>Yelm, WA</a></li>
+          <li><a href='tel:206-578-6741'>206-578-6741</a></li>
+          <li><a href="mailto:cameron@dansclan.com">cameron@dansclan.com</a></li>
+          <li><a href="https://www.linkedin.com/in/cameron-millen/">Linkedin</a></li>
+          <li><a href="https://github.com/FTWCougar">Github</a></li>
+          <li><a href="https://dev.to/ftwcougar">Blog</a></li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
